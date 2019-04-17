9c761f3ca98f3252670cb09ab7aeb9c8ac0633dd
monsterthirteen <809609345@qq.com>
2019-04-17 16:26:44 +0800

    1.0.0

---
+++ precache-manifest.fd2306deec31819befed4964549382c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6dc1bf7100b76c7a5fec",
+    "url": "/static/css/main.4565abee.chunk.css"
+  },
+  {
+    "revision": "6dc1bf7100b76c7a5fec",
+    "url": "/static/js/main.4fdcd8d5.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "9349c269438cb7695e70",
+    "url": "/static/css/2.bd76bc75.chunk.css"
+  },
+  {
+    "revision": "9349c269438cb7695e70",
+    "url": "/static/js/2.b35b6026.chunk.js"
+  },
+  {
+    "revision": "2ba464265473eb2525d8e91843ef2903",
+    "url": "/index.html"
+  }
+];
